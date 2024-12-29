eff7071aff37a89ccd4488d69aa2c61db0c19fd2
Sura Itana <suraitana@gmail.com>
2024-12-29 16:23:20 +0300

    connect to db
    add local mongodb connection

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./utils/db'); 
+
+
+dotenv.config();
+connectDB();
+
 const app = express();
-
-
-
 const port = 3000;
 
 app.get('/', (req, res) => {
